6201afd50c20b7c02c6f87688b1133da68deca83
youking-lib <whistleyz@163.com>
2023-07-23 22:02:08 +0800

    chore: prune dead code

--- src/plugin-markdown/encoding.ts
+++ src/plugin-markdown/encoding.ts
@@ -29,7 +29,31 @@ export function markdownToHtml(
   markdown: string,
   { codeHighlight = false }: MarkdownToHtmlOptions = {}
 ) {
-  const u = unified().use(remarkParse).use(remarkGfm).use(remarkRehype);
+  const u = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(() => (mdast: Root) => {
+      visit(mdast, 'element', function (node, _) {
+        if (node['tagName'] === 'li') {
+          const classes = node['properties']['className'] as string[];
+
+          if (classes && classes.includes('task-list-item')) {
+            // @ts-ignore
+            const input = node.children[0];
+            const label = {
+              children: [input],
+              properties: {},
+              tagName: 'label',
+              type: 'element'
+            };
+
+            // @ts-ignore
+            node.children[0] = label;
+          }
+        }
+      });
+    });
 
   if (codeHighlight) {
     u.use(rehypeHighlight);
