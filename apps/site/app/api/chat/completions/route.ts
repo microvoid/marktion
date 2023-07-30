4fa236699cb8cc906cd6816066a116fad51b72af
youking-lib <whistleyz@163.com>
2023-07-30 22:03:33 +0800

    chore: cors for dev

--- apps/site/app/api/chat/completions/route.ts
+++ apps/site/app/api/chat/completions/route.ts
@@ -38,7 +38,19 @@ export async function POST(req: Request): Promise<Response> {
 
   // temp
   response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   // Respond with the stream
   return response;
 }
+
+export const OPTIONS = () =>
+  new Response('ok', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    }
+  });
