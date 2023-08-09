1ce743004368ee74caecadc6db9d612fa972e03f
youking-lib <whistleyz@163.com>
2023-08-09 11:38:18 +0800

    feat: clear style

--- examples/with-vite/src/App.tsx
+++ examples/with-vite/src/App.tsx
@@ -1,40 +1,35 @@
-import { useMemo, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useMemo, useRef, useState } from 'react';
 import { getPlugins } from './plugins';
-import { Marktion } from '../../../dist';
+import { Marktion, MarktionRef } from '../../../dist';
 
-import './App.css';
+import '../../../dist/style.css';
 
 const INIT_MARKDOWN = [import.meta.env.VITE_README_ZH, import.meta.env.VITE_README_EN];
 
 function App() {
-  const [count, setCount] = useState(0);
   const [lang, setLang] = useState(0);
   const plugins = useMemo(() => getPlugins(), []);
+  const marktionRef = useRef<MarktionRef>(null);
 
   return (
-    <>
+    <div style={{ padding: 25 }}>
+      <button
+        style={{ marginBottom: 10 }}
+        onClick={() => {
+          const index = Number(!Boolean(lang));
+
+          setLang(index);
+
+          marktionRef.current?.editor.commands.setMarkdwon(INIT_MARKDOWN[index]);
+        }}
+      >
+        中文/English
+      </button>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Marktion ref={marktionRef} markdown={INIT_MARKDOWN[lang]} plugins={plugins} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div>
-        <Marktion markdown={INIT_MARKDOWN[lang]} plugins={plugins} />
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
