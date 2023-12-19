fae41125ef6dec44f95962d349136bac5aad05b5
youking-lib <whistleyz@163.com>
2023-12-19 14:19:13 +0800

    fix: tailwind include clients dir

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './clients/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
