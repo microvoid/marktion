472419b8f88d227776ea4ea71d427eefe40ed62b
youking-lib <whistleyz@163.com>
2024-02-19 15:37:23 +0800

    fix(site): ts error

--- apps/site/app/api/statistics/project/[id]/route.ts
+++ apps/site/app/api/statistics/project/[id]/route.ts
@@ -2,7 +2,7 @@ import { ApiUtils, AuthHelper } from '@/libs';
 import { statsHelper } from '@/libs/helpers';
 
 export const GET = AuthHelper.validate<{ id: string }>(async (req, ctx) => {
-  const userId = ctx.params.id;
+  const userId = ctx.params?.id!;
   const userStatics = await statsHelper.getProjectStatistics(userId);
 
   return ApiUtils.success(userStatics);
