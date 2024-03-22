76122cf0ae4e8c826e477ce9230999d2af12907e
youking-lib <whistleyz@163.com>
2024-03-22 10:54:12 +0800

    feat(marktion): init toolbar

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['marktion'],
   images: {
     domains: ['avatars.githubusercontent.com', 'cdn.marktion.io']
   }
--- packages/marktion/src/react-components/codemirror/codemirror.tsx
+++ packages/marktion/src/react-components/codemirror/codemirror.tsx
@@ -1,7 +1,9 @@
-import React, { useMemo, useState } from 'react';
-import { Updater, useImmer } from 'use-immer';
-import { codemirror } from '../../plugin-codemirror';
 import ReactDOM, { createPortal } from 'react-dom';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Updater, useImmer } from 'use-immer';
+import { Select } from 'antd';
+
+import { codemirror, CodeMirrorNodeView } from '../../plugin-codemirror';
 
 export function useCodemirror() {
   const [toolbars, setToolbars] = useState<React.ReactNode[]>([]);
@@ -24,12 +26,17 @@ export function useCodemirror() {
 
         function Wrapper() {
           const [setting, dispath] = useImmer<ToolbarProps['setting']>({
-            lang: node.getLanguage()
+            lang: node.getLanguage(),
+            langs: node.getLanguages()
           });
 
           dispathRef.current = dispath;
 
-          return <Toolbar setting={setting} />;
+          useEffect(() => {
+            node.setLanguage(setting.lang);
+          }, [setting]);
+
+          return <Toolbar setting={setting} dispatch={dispath} />;
         }
 
         return {
@@ -47,8 +54,6 @@ export function useCodemirror() {
     });
   }, []);
 
-  console.log('toolbars', toolbars);
-
   return useMemo(() => {
     return {
       plugin,
@@ -57,12 +62,69 @@ export function useCodemirror() {
   }, [toolbars]);
 }
 
-type ToolbarProps = {
-  setting: {
-    lang: string;
-  };
+type ToolbarSetting = {
+  lang: string;
+  langs: ReturnType<CodeMirrorNodeView['getLanguages']>;
 };
 
-function Toolbar({ setting }: ToolbarProps) {
-  return <span>{setting.lang}</span>;
+type ToolbarProps = {
+  setting: ToolbarSetting;
+  dispatch: Updater<ToolbarSetting>;
+};
+
+function Toolbar({ setting, dispatch }: ToolbarProps) {
+  const [state, setState] = useImmer({
+    isFocus: false
+  });
+
+  return (
+    <Select
+      onFocus={() =>
+        setState(draft => {
+          draft.isFocus = true;
+        })
+      }
+      onBlur={() =>
+        setState(draft => {
+          draft.isFocus = false;
+        })
+      }
+      showSearch
+      size="small"
+      variant="filled"
+      placeholder="Select a language"
+      optionFilterProp="children"
+      placement="bottomRight"
+      style={{
+        minWidth: state.isFocus ? 100 : undefined
+      }}
+      dropdownStyle={{
+        minWidth: state.isFocus ? 100 : undefined
+      }}
+      value={setting.lang}
+      onChange={value => {
+        dispatch(draft => {
+          draft.lang = value;
+        });
+      }}
+      filterOption={(input, option) => {
+        const filters = [option?.label, ...(option?.alias || [])];
+        return Boolean(filters.find(item => item?.includes(input)));
+      }}
+      filterSort={(optionA, optionB) =>
+        (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+      }
+      options={useMemo(
+        () =>
+          setting.langs.map(item => {
+            return {
+              alias: item.alias,
+              value: item.name,
+              label: item.name
+            };
+          }),
+        [setting.langs]
+      )}
+    />
+  );
 }
